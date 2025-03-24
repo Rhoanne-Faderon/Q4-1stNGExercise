7768b909288b7ac16da4c8bd4735193b8c02b3f1
Rhoanne-Faderon <rhoanneungsodfaderon@gmail.com>
2025-03-24 22:18:50 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,31 +1,37 @@
-// Loads the express module
-const express = require("express");
-const hbs = require("hbs");
-
-const bodyParser = require("body-parser");
-
-const path = require("path");
-
-//Creates our express server
+const express = require('express');
+const path = require('path');
 const app = express();
-const port = 3000;
+const PORT = 3000;
 
-//Serves static files (we need it to import a css file)
-app.use(express.static(path.join(__dirname, "public")));
-app.set("view engine", "hbs");
-app.use(bodyParser.urlencoded({ extended: true }));
 
-//Sets a basic route
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public'))); 
 
-// Render the initial page with the number input form
-app.get("/", (req, res) => {
-  res.render("index");
+
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+
+
+app.get('/', (req, res) => {
+    res.render('index'); 
 });
 
-// Create express route binder for draw.hbs and get the data from the url as parameters
-// that came from index.hbs
+app.get('/draw', (req, res) => {
+    const sideLength = parseFloat(req.query.sideLength);
+
+    if (isNaN(sideLength) || sideLength <= 0) {
+        return res.render('draw', { error: 'Please enter a valid positive number.' });
+    }
+
+    res.render('draw', { sideLength });
+});
 
 
+app.use((req, res) => {
+    res.status(404).send('<h1>404 - Page Not Found</h1>');
+});
 
-//Makes the app listen to port 3000
-app.listen(port, () => console.log(`App listening to port ${port}`));
+
+app.listen(PORT, '127.0.0.1', () => {
+    console.log(`Server running at http://127.0.0.1:${PORT}/`);
+});
